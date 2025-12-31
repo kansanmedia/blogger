1c53ea938b938bb997033c7d637cefadd119d709
kansanmedia <kansanmedia@gmail.com>
2025-12-31 16:45:19 +0530

    Update Firebase SDK version and refactor code

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,9 +1,8 @@
-// Import and initialize the Firebase SDK
-importScripts('https://www.gstatic.com/firebasejs/9.15.0/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/9.15.0/firebase-messaging-compat.js');
+/* firebase-messaging-sw.js content */
+importScripts('https://www.gstatic.com/firebasejs/9.22.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.22.0/firebase-messaging-compat.js');
 
-// यहाँ पर अपना Firebase Config कोड डालें
-const firebaseConfig = {
+firebase.initializeApp({
     apiKey: "AIzaSyDxGYPmpw6Ag8Zd_qTym05rekSj_tWxRkI",
     authDomain: "blogger-login-b9795.firebaseapp.com",
     projectId: "blogger-login-b9795",
@@ -11,18 +10,17 @@ const firebaseConfig = {
     messagingSenderId: "704318869373",
     appId: "1:704318869373:web:f8e477147de540336a8614",
     measurementId: "G-49THRS5MVZ"
-  };
+});
 
-firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
-// Handle background messages
-messaging.onBackgroundMessage(function(payload) {
+// Optional: Background handler
+messaging.onBackgroundMessage((payload) => {
   console.log('[firebase-messaging-sw.js] Received background message ', payload);
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: "<data:blog.blogspotFaviconUrl/>" // Dynamic favicon के लिए
+    icon: '/favicon.ico'
   };
 
   self.registration.showNotification(notificationTitle, notificationOptions);
